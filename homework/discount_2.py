13d2e11a167e815c56e9911b5186712abe9a234f
“John <john@email.com>
2026-04-01 00:25:04 +0300

    Did my homework

---
+++ homework/discount_2.py
@@ -0,0 +1,2 @@
+DISCOUNT_TRIGGER_PEOPLE = 30  # знижка, якщо більше 30 людей
+DISCOUNT_PERCENTAGE = 10      # 10% знижки
---
+++ homework/prices_2.py
@@ -0,0 +1,4 @@
+PRICE_BUS = 5000       # за 1 автобус
+PRICE_TRAIN = 300      # за 1 особу
+PRICE_HOTEL_NIGHT = 400 # за 1 особу/ніч
+BUS_CAPACITY = 40      # місць в одному автобусі
