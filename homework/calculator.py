4b0ba5c15545180ec7019fd1ed28cb42e657ad86
“John <john@email.com>
2026-04-02 23:07:24 +0300

    some files

--- homework/calculator.py
+++ homework/calculator.py
@@ -1,23 +1,25 @@
-from pywebio.input import input
+from pywebio.input import input, select
 from pywebio.output import put_markdown
 while True:
     number_1 = input("Введи первое число:").strip()
+    count = select("Введи действие:", ['+', '-', '*', '/'])
     number_2 = input("Введи второе число:").strip()
     if number_1.isdigit():
         number_1 = int(number_1)
         if number_2.isdigit():
             number_2 = int(number_2)
-            suma = number_1 + number_2
-            minus = number_1 - number_2
-            multiply = number_1 * number_2
-            podelit = number_1 / number_2
-            put_markdown("## Ответ:")
-            put_markdown("---")
-            put_markdown(f"# Сума: {suma}")
-            put_markdown(f"# Вычитание: {minus}")
-            put_markdown(f"# Умножение: {multiply}")
-            put_markdown(f"# Деление: {podelit}")
-            put_markdown("---")
+            if count == "+":
+                suma = number_1 + number_2
+                put_markdown(f"## {number_1} + {number_2} = {suma}")
+            if count == "-":
+                minus = number_1 - number_2
+                put_markdown(f"## {number_1} - {number_2} = {minus}")
+            if count == "*":
+                multiply = number_1 * number_2
+                put_markdown(f"## {number_1} * {number_2} = {multiply}")
+            if count == "/":
+                podelit = number_1 / number_2
+                put_markdown(f"## {number_1} / {number_2} = {podelit}")
         else:
             put_markdown("## Нормально напиши")
             put_markdown("---")
