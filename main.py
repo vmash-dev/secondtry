a2002546be27203a772dca1f210571f284442ee8
“John <john@email.com>
2026-05-11 20:08:41 +0300

    + get_weather_info

---
+++ main.py
@@ -0,0 +1,5 @@
+import utils
+
+
+current_weather = utils.get_weather_info("Одеса")
+print(current_weather)
--- utils.py
+++ utils.py
@@ -10,11 +10,22 @@ def get_weather_info(city: str) -> dict:
     }
     response = requests.get(constants.OPEN_WEATHER_API_URL, params=params)
     response_json = response.json()
-    print(response.url, response_json)
-    return {}
+    # print(response.url, response_json)
+    temperature = response_json["main"]["temp"]
+    temperature_like = response_json["main"]["feels_like"]
+    weather_main = response_json["weather"][0]["main"]
+    weather_description = response_json["weather"][0]["description"]
+    wind_speed = response_json["wind"]["speed"]
+    return {
+        "temperature": temperature,
+        "temperature_like": temperature_like,
+        "weather_main": weather_main,
+        "weather_description": weather_description,
+        "wind_speed": wind_speed,
+    }
 
 
 
-get_weather_info("Одеса")
+
 
 
