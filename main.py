3d0641c6bf410699ec87ba817c288045a5446cd8
vasylkartychak <w.i.k.mailua@gmail.com>
2026-05-14 19:43:50 +0300

    server

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import utils
-from pywebio.input import input
+from pywebio.input import input, input_group
 from pywebio.output import put_text
+from pywebio import start_server
+from pywebio.session import run_js
 
 
 def main():
-    while True:
-        city = input("City: ")
-        current_weather = utils.get_weather_info(city)
-        put_text(f"Температура у {city}: {current_weather['temperature']}")
 
-main()
+    data = input_group(
+        "Запит на поточну погоду",
+        [
+            input("City", name="city", required=True),
+            input("Email", name="email", required=True),
+            input("Name", name="name", required=True),
+        ]
+    )
+    print(data)
+
+    run_js("""
+        setTimeout(() => {
+            window.location.reload();
+        }, 5000);
+    """)
+    # current_weather = utils.get_weather_info(city)
+    # put_text(f"Температура у {city}: {current_weather['temperature']}")
+
+
+start_server(
+    main,
+    host="0.0.0.0",
+    port=8888,
+    debug=True,
+)
 
--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 from typing import Any
 from datetime import datetime
 
+# import os
+# import smtplib
+# from email import encoders
+# from email.mime.base import MIMEBase
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+
+
 import requests
 import constants
 import config
@@ -34,3 +42,51 @@ def get_weather_info(city: str) -> dict:
     }
     write_logs('get_weather_info', {"city": city}, result)
     return result
+
+
+
+
+
+# def send_email(
+#     recipients: list[str],
+#     /,
+#     *,
+#     mail_body: str,
+#     mail_subject: str,
+#     attachment: str = None,
+# ):
+#     TOKEN = settings.TOKEN_UKR_NET
+#     USER = settings.USER
+#     SMTP_SERVER = settings.SMTP_SERVER
+#
+#     msg = MIMEMultipart('alternative')
+#     msg['Subject'] = mail_subject
+#     msg['From'] = f'<Email was sent from {USER}>'
+#     msg['To'] = ', '.join(recipients)
+#     msg['Reply-To'] = USER
+#     msg['Return-Path'] = USER
+#     msg['X-Mailer'] = 'decorator'
+#
+#     # text_to_send = MIMEText(mail_body, 'plain')
+#     text_to_send = MIMEText(mail_body, 'html')
+#     msg.attach(text_to_send)
+#
+#     if attachment:
+#         is_file_exists = os.path.exists(attachment)
+#         if is_file_exists:
+#             basename = os.path.basename(attachment)
+#             filesize = os.path.getsize(attachment)
+#             file = MIMEBase('application', f'octet-stream; name={basename}')
+#             file.set_payload(open(attachment, 'br').read())
+#             file.add_header('Content-Description', attachment)
+#             file.add_header(
+#                 'Content-Description',
+#                 f'attachment; filename={attachment}, size={filesize}',
+#             )
+#             encoders.encode_base64(file)
+#             msg.attach(file)
+#
+#     mail = smtplib.SMTP_SSL(SMTP_SERVER)
+#     mail.login(USER, TOKEN)
+#     mail.sendmail(USER, recipients, msg.as_string())
+#     mail.quit()
