e47c09ec2b42853406de385d351dbe76ebe41250
“John <john@email.com>
2026-06-01 21:41:27 +0300

    some files

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ import os
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+
+TOKEN_UKR_NET = os.getenv("TOKEN_UKR_NET")
+USER_UKR_NET = os.getenv("USER_UKR_NET")
+SMTP_SERVER = os.getenv("SMTP_SERVER")
